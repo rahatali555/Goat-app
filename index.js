594f6ccc680422cfb9d4d221c758f5fb90ac31fe
Rahat Ali <ali.rahat555@gmail.com>
2025-09-02 13:02:25 +0500

    added feed recipes (ratio must sum to 1)

--- index.js
+++ index.js
@@ -22,3 +22,28 @@ document.getElementById("goatForm").addEventListener("submit", function(event) {
     <p>📋 Feeding plan will be calculated here!</p>
   `;
 });
+// Local mix recipes (ratios must sum to 1)
+const MIXES={
+grower:{
+  wheat_Bran:0.40, //Chokar
+  crushed_maize:0.30,//Makai Dana
+  cottonseed_cake:0.20, // Khal Binola(or sarsun khal / Chana Chiri)
+  rice_polish:0.08,// chawal Chokar
+  mineral_salt:0.02 //~ 1.5% mineral + 0.5% salt
+}
+,
+lactation:{
+wheat_bran: 0.30,
+crushed_maize:0.25,  
+oilseed_cake: 0.35,   // Sarson Khal / Binola Khal
+rice_polish: 0.08,
+mineral_salt: 0.02
+},
+fattening:{
+    crushed_maize: 0.45,
+    wheat_bran: 0.25,
+    oilseed_cake: 0.25,
+    molasses:0.03, // Sheera (optional)
+     mineral_salt: 0.02
+}
+};
